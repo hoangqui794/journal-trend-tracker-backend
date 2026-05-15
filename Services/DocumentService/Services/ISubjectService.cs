e1fa2c8ac61b6f1b9dd524d6c2e45618218770b3
Cao Hữu Trí <caohuutritl1234@gmail.com>
2026-05-15 07:58:18 +0700

    refactor: remove unused legacy authentication modules

--- Services/DocumentService/Services/ISubjectService.cs
+++
@@ -1,9 +0,0 @@
-using DocumentService.DTOs.Responses;
-
-namespace DocumentService.Services
-{
-    public interface ISubjectService
-    {
-        Task<IEnumerable<SubjectResponse>> GetAllSubjectsAsync();
-    }
-}
